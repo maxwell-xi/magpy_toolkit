caaa8fcbcdb81c32a3a5b634b663cc3a94f5cc61
unknown <xi@itis.swiss>
2024-12-12 21:29:58 +0100

    make ggsm functions accept single-data-point-type gradient

--- ggsm.py
+++ ggsm.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 def induced_e_cube(g_n, field, e_br, hfield_used=True): # valid for ICNIRP 2010 & ICNIRP 2020
-    if (g_n < 0).any():
-        print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
-        g_n = np.abs(g_n)
+    if isinstance(g_n, (int, float)):
+        if g_n < 0:
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)            
+    else:
+        if (g_n < 0).any():
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)
     k = 1/((1+6.5e-6*g_n**5.8)**(1/5.8))
     if hfield_used == True:
         e_est = k * field / 21 * e_br # H0 = 21 A/m
@@ -12,9 +17,14 @@ def induced_e_cube(g_n, field, e_br, hfield_used=True): # valid for ICNIRP 2010 
     return e_est
 
 def induced_e_line(g_n, field, e_br, hfield_used=True): # valid for IEEE 2005 & IEEE 2019
-    if (g_n < 0).any():
-        print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
-        g_n = np.abs(g_n)
+    if isinstance(g_n, (int, float)):
+        if g_n < 0:
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)            
+    else:
+        if (g_n < 0).any():
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)
     k = 1/((1+4e-11*g_n**6.6)**(1/6.6))
     if hfield_used == True:
         e_est = k * field / 163 * e_br # H0 = 163 A/m
@@ -23,9 +33,14 @@ def induced_e_line(g_n, field, e_br, hfield_used=True): # valid for IEEE 2005 & 
     return e_est
    
 def induced_j_area(g_n, field, j_br, hfield_used=True): # valid for ICNIRP 1998
-    if (g_n < 0).any():
-        print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
-        g_n = np.abs(g_n)
+    if isinstance(g_n, (int, float)):
+        if g_n < 0:
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)            
+    else:
+        if (g_n < 0).any():
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)
     k = 1/((1+4e-3*g_n**2.9)**(1/2.9))
     if hfield_used == True:
         j_est = k * field / 5 * j_br # H0 = 5 A/m
@@ -34,9 +49,14 @@ def induced_j_area(g_n, field, j_br, hfield_used=True): # valid for ICNIRP 1998
     return j_est
     
 def sar_10g(g_n, field, f, hfield_used=True): # valid for ICNIRP 1998/2020 and IEEE 2005/2019
-    if (g_n < 0).any():
-        print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
-        g_n = np.abs(g_n)
+    if isinstance(g_n, (int, float)):
+        if g_n < 0:
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)            
+    else:
+        if (g_n < 0).any():
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)
     k = ( 1/((1+2e-1*g_n**1.2)**(1/1.2)) )**2
     if hfield_used == True:
         sar_est = k * (field/5)**2 * (f/100e3)**2 * 2 * 2e-4 # H0 = 5 A/m, SAR_BR = 2 W/kg
@@ -45,9 +65,14 @@ def sar_10g(g_n, field, f, hfield_used=True): # valid for ICNIRP 1998/2020 and I
     return sar_est
 
 def sar_1g(g_n, field, f, hfield_used=True): # valid for FCC and ISED
-    if (g_n < 0).any():
-        print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
-        g_n = np.abs(g_n)
+    if isinstance(g_n, (int, float)):
+        if g_n < 0:
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)            
+    else:
+        if (g_n < 0).any():
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)
     k = ( 1/((1+2.5e-1*g_n**1.1)**(1/1.1)) )**2
     if hfield_used == True:
         sar_est = k * (field/1.63)**2 * (f/100e3)**2 * 1.6 * 4.6e-5 # H1 = 1.63 A/m, SAR_BR = 1.6 W/kg
@@ -56,9 +81,14 @@ def sar_1g(g_n, field, f, hfield_used=True): # valid for FCC and ISED
     return sar_est
 
 def induced_e_local(g_n, field, e_br, hfield_used=True): # valid for ISED
-    if (g_n < 0).any():
-        print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
-        g_n = np.abs(g_n)
+    if isinstance(g_n, (int, float)):
+        if g_n < 0:
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)            
+    else:
+        if (g_n < 0).any():
+            print('Negative normalized gradent encountered! Absolute value used to derive the induced field.')
+            g_n = np.abs(g_n)
     k = 1/((1+6.5e-6*g_n**5.8)**(1/5.8))
     if hfield_used == True:
         e_est = k * field / 21 * e_br # H0 = 21 A/m
